4d52d022dc60c0931a60b177c791de3a7a8ceac7
Ujjawal Modanwal <51051403+ujjawalmodanwal@users.noreply.github.com>
2022-12-26 00:58:10 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   	return (
     	<div className="App">
 			<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"></meta>
-			<BrowserRouter>
+			<BrowserRouter basename='/'>
 				<Routes>
           			<Route path="/" element = {<Welcome/>} />
 					<Route path="/Login" element = {<Login/>} />
